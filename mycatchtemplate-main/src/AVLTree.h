d1102a97afd3f4e4102ce6ebcc99e82eba996481
U-DESKTOP-PIOA5NP\Caleb Schutz <cschutz@ufl.edu>
2024-09-25 10:08:27 -0700

    third commit

--- mycatchtemplate-main/src/AVLTree.h
+++ mycatchtemplate-main/src/AVLTree.h
@@ -4,54 +4,84 @@
 #include<string>
 
 struct AVLTree {
-    struct TreeNode{
+    struct TreeNode {
         //Initialize node variables
-        TreeNode* leftNode;
-        TreeNode* rightNode;
-        std::string name;
-        int id;
+        TreeNode *leftNode = nullptr;
+        TreeNode *rightNode = nullptr;
+        std::string name = "";
+        int height = -10;
+        int id = -1;
 
-        TreeNode(std::string name, int id){
+        TreeNode(std::string name, int id) {
             this->name = name;
             this->id = id;
         }
-        ~TreeNode(){
+
+        ~TreeNode() {
             delete this;
         }
-
     };
+
     //Initialize tree variables
-    TreeNode* head = nullptr;
+    TreeNode *head = nullptr;
 
     //Default constructor
-    AVLTree(){
-
+    AVLTree() {
     }
 
     //functions
-    void searchID(int ID){
+    void searchID(int ID) {
         //use the ID provided to search for the student within the tree
         //if the ID is found print their name
         //otherwise print unsuccessful
     }
-    void insert(std::string NAME, int ID){
+
+    void insert(std::string NAME, int ID) {
         //the id must be unique use searchID
         //add a student object into the tree with the NAME and ID provided
         //then balance the tree and print successful
         //Note NAME will contain the "Caleb Schutz" when passed in
-        if(head == nullptr){
-            TreeNode* newHead = new TreeNode(NAME, ID);
+        TreeNode* it = head;
+        if (head == nullptr) {
+            TreeNode *newHead = new TreeNode(NAME, ID);
+            newHead->height = 0;
             head = newHead;
+            return;
+        }
+        while(true) {
+            if(it->id == ID) {//make sure it isn't already in the tree
+                std::cout << "unsuccessful (id is already in list)" << std::endl;
+                break;
+            }
+            if(ID > it->id) {//if ID is greater than the current id
+                if(it->rightNode == nullptr) {
+                    it->rightNode = new TreeNode(NAME, ID);
+                    it->rightNode->height = it->height - 1;//adjust height when placed
+                }
+                else {
+                    it = it->rightNode;
+                }
+            }
+            else {//if ID is less than the current id
+                if(it->leftNode == nullptr) {
+                    it->leftNode = new TreeNode(NAME, ID);
+                    it->leftNode->height = it->height - 1;//adjust height when placed
+                }
+                else {
+                    it = it->leftNode;
+                }
+            }
         }
 
     }
-    void remove(int ID){
+
+    void remove(int ID) {
         //if id doesn't exist within the tree print unsuccessful
         //find and remove the account with the specified ID
         //balance the tree using the INORDER successor if necessary
-
     }
-    void search(std::string NAME){
+
+    void search(std::string NAME) {
         /*
          Search for the student in the tree with the specified name, NAME.
          If the student's name was found, print the associated ID.
@@ -59,27 +89,48 @@ struct AVLTree {
          If the name does not exist within the tree, print “unsuccessful.”
          NAME identifier will be surrounded by double quotes for parsing, e.g., "Josh Smith."
         */
+    }
 
-    }
-    void printInOrder(){
+    void printInOrder(TreeNode* head) {
         //print a comma seperated inorder traversal of the names
+        if(head == nullptr) {
+            return;
+        }
+        printInOrder(head->leftNode);
+        std::cout << head->name << ", ";
+        printInOrder(head->rightNode);
     }
-    void printPreOrder(){
+
+    void printPreOrder(TreeNode* head) {
         //print a comma seperated preorder traversal of the names
+        if(head == nullptr) {
+            return;
+        }
+        std::cout << head->name << ", ";
+        printPreOrder(head->leftNode);
+        printPreOrder(head->rightNode);
     }
-    void printPostOrder(){
+
+    void printPostOrder() {
         //print a comma seperated postorder traversal of the names
+        if(head == nullptr) {
+            return;
+        }
+        printPreOrder(head->leftNode);
+        printPreOrder(head->rightNode);
+        std::cout << head->name << ", ";
     }
-    void printLevelCount(){
+
+    void printLevelCount() {
         //print the number of levels that exist in the tree
         //print 0 if the head of the tree is null
     }
-    void removeInOrderN(int N){
+
+    void removeInOrderN(int N) {
         // remove the Nth GatorID from the inorder traversal of the tree
         //balance the tree
         // if the removal is successful print successful otherwise print unsuccessful
     }
-
 };
 
 
--- mycatchtemplate-main/src/main.cpp
+++ mycatchtemplate-main/src/main.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include "AVLTree.h"
 
-int main(){
+int main() {
     //Get the number of commands and turn it into an integer
     std::string numOfCommandsString;
     std::getline(std::cin, numOfCommandsString);
@@ -16,84 +16,104 @@ int main(){
     std::string command;
     std::vector<std::string> commandList;
 
-    for(int i = 0; i < numOfCommands; i++){
+    for (int i = 0; i < numOfCommands; i++) {
         //get the command and separate it by spaces
         std::getline(std::cin, command);
         int index = 0;
-        for(int j = 0; j < (int) command.size(); j++){
-            if (char(command[j]) == ' ' && (char(command[j + 1]) == ' ')){ //make sure there aren't multiple spaces in a row
+        for (int j = 0; j < (int) command.size(); j++) {
+            if (char(command[j]) == ' ' && (char(command[j + 1]) == ' ')) {
+                //make sure there aren't multiple spaces in a row
                 std::cout << "unsuccessful" << std::endl;
                 break;
             }
-            if(char(command[j]) == '"'){ //if the command is a name then don't separate at the space
-                while(j < (int) command.size()){
+            if (char(command[j]) == '"') {
+                //if the command is a name then don't separate at the space
+                while (j < (int) command.size()) {
                     j++;
-                    if(!isalpha(char(command[j])) && char(command[j]) != ' ' && char(command[j]) != '"'){//if any of the characters between the parenthesis aren't alphabetical or a space
+                    if (!isalpha(char(command[j])) && char(command[j]) != ' ' && char(command[j]) != '"') {
+                        //if any of the characters between the parenthesis aren't alphabetical or a space
                         commandList[0] = " ";
                         break;
                     }
-                    if(char(command[j]) == '"'){
+                    if (char(command[j]) == '"') {
                         j++;
                         commandList.push_back(command.substr(index, j - index));
                         index = j + 1;
-                        std::cout << index << std::endl;
+                        //std::cout << index << std::endl;
                         break;
-                    }else if(j == (int) command.size()){//if the parenthesis are never closed break and make sure it returns unsuccessful
+                    } else if (j == (int) command.size()) {
+                        //if the parenthesis are never closed break and make sure it returns unsuccessful
                         commandList[0] = " ";
                         break;
                     }
                 }
-            }else if (char(command[j]) == ' '){
+            } else if (char(command[j]) == ' ') {
                 commandList.push_back(command.substr(index, j - index));
                 index = j + 1;
                 continue;
-            }else if(j == ((int) command.size()) - 1){//this is for the last command because there is no space to remove
+            } else if (j == ((int) command.size()) - 1) {
+                //this is for the last command because there is no space to remove
                 commandList.push_back(command.substr(index, j - index + 1));
             }
         }
         /*test commandList
         for(auto v: commandList){
             std::cout << v << std::endl;
-        }
-         */
+        }*/
+
 
 
         //Do stuff based off the command
-        if(commandList[0] == "insert"){
+        if (commandList[0] == "insert") {
             //make sure there are only 3 commands
-            if(commandList.size() > 3 || commandList[2].size() != 8){ //make sure there are the right amount of command and the ID is the right length
-                std::cout << "unsuccessful" << std::endl;
+            if (commandList.size() > 3 || commandList[2].size() != 8) {
+                //make sure there are the right amount of command and the ID is the right length
+                std::cout << "unsuccessful size/commandlength" << std::endl;
+                index = 0;
+                commandList.clear();
                 continue;
+
             }
+            for(auto n: commandList[2]){
+                if(!std::isdigit(n)){//if the 3rd command contains a non digit
+                    std::cout << "unsuccessful digit" << std::endl;
+                    index = 0;
+                    commandList.clear();
+                    continue;
+                }
+            }
+            //std::cout << commandList[2];
             studentTree.insert(commandList[1], std::stoi(commandList[2]));
+        } else if (commandList[0] == "remove") {
 
-        }else if(commandList[0] == "remove"){
+        } else if (commandList[0] == "search") {
 
-        }else if(commandList[0] == "search"){
             //check if you are searching for id or name first
-            if(std::isalpha(commandList[1][0])){
+            if (std::isalpha(commandList[1][0])) {
                 //search for name
-            }else{
+            } else {
                 //search for ID
             }
-        }else if(commandList[0] == "printInorder"){
+        } else if (commandList[0] == "printInorder") {
+            studentTree.printInOrder(studentTree.head);
 
-        }else if(commandList[0] == "printPreorder"){
+        } else if (commandList[0] == "printPreorder") {
 
-        }else if(commandList[0] == "printPostorder"){
+        } else if (commandList[0] == "printPostorder") {
 
-        }else if(commandList[0] == "printLevelCount"){
+        } else if (commandList[0] == "printLevelCount") {
 
-        }else if(commandList[0] == "removeInorder"){
+        } else if (commandList[0] == "removeInorder") {
 
-        }else{
+        } else {
             std::cout << "unsuccessful" << std::endl;
         }
 
         //Reset variables between command inputs
-        index = 0;//reset index
-        commandList.clear();//clear the commandList for the next command
+        index = 0; //reset index
+        commandList.clear(); //clear the commandList for the next command
+        commandList[0] = "";//do this for some reason (possibly undefined behavior)
     }
 
-	return 0;
+    return 0;
 }
